8fb974b7e42075226775dd0feff6f5df5af40d17
Anthony Musiri <musiritapia@gmail.com>
2026-05-02 15:54:19 -0500

    Fix Program.cs error de compilacion

--- Program.cs
+++ Program.cs
@@ -1,28 +1,42 @@
+using ManoloCRM.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 builder.Services.AddSession(options =>
 {
-options.IdleTimeout = TimeSpan.FromMinutes(30);
-options.Cookie.HttpOnly = true;
-options.Cookie.IsEssential = true;
+    options.IdleTimeout = TimeSpan.FromMinutes(30);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
 });
+
 var app = builder.Build();
-// Crear y migrar la BD automáticamente al iniciar
+
 using (var scope = app.Services.CreateScope())
 {
-var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-db.Database.Migrate();
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
 }
+
 if (!app.Environment.IsDevelopment())
 {
-app.UseExceptionHandler("/Home/Error");
-app.UseHsts();
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
 }
+
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
-app.UseSession();       // <-- IMPORTANTE: antes de Authorization
+app.UseSession();
 app.UseAuthorization();
-// La ruta por defecto va al Login
+
 app.MapControllerRoute(
-name: "default",
-pattern: "{controller=Account}/{action=Login}/{id?}");
+    name: "default",
+    pattern: "{controller=Account}/{action=Login}/{id?}");
+
 app.Run();
